477fa842756052dc67332de0339886f4bf3e5910
Nadeŭka <me@nadevko.cc>
2025-10-12 18:07:00 +0300

    already have autoprefixes

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,6 +1,5 @@
-import autoprefixer from "autoprefixer";
 import postcssPresetEnv from "postcss-preset-env";
 
 export default {
-  plugins: [autoprefixer(), postcssPresetEnv({ stage: 0 })],
+  plugins: [postcssPresetEnv({ stage: 0 })],
 };
